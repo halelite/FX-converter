7afc8daae6fb165c6d6f8e963f98df1d1f2a345d
hale <hale.sheikhi81@gmail.com>
2026-07-11 02:34:54 +0330

    feat(ui): handle responsiveness

--- src/app/components/detailsContainer.tsx
+++ src/app/components/detailsContainer.tsx
@@ -1,33 +1,75 @@
+"use client";
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import History from "./history";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useState } from "react";
+import Compare from "./compare";
+import Favorites from "./favorites";
+import Logs from "./logs";
+import { useFavorites } from "../context/favoritesContext";
 
-const DetailsContainer = () => {
+type DetailsProps = {
+  baseCurrency: string;
+  toCurrency: string;
+  amount: number;
+};
+
+const DetailsContainer = ({
+  baseCurrency,
+  toCurrency,
+  amount,
+}: DetailsProps) => {
+  const [selectedTab, setSelectedTab] = useState("history");
+  const { favorites } = useFavorites();
+
   const tabItems = [
     {
       value: "history",
       title: "HISTORY",
-      content: <History />,
+      content: <History baseCurrency={baseCurrency} toCurrency={toCurrency} />,
     },
     {
       value: "compare",
       title: "COMPARE",
-      content: <div>COMPARE CONTENT</div>,
+      content: <Compare baseCurrency={baseCurrency} amount={amount} />,
     },
     {
       value: "favorites",
-      title: "FAVORITES",
-      content: <div>FAVORITES CONTENT</div>,
+      title: (
+        <div className="me-6 w-full flex items-center justify-between gap-2 sm:me-0">
+          <span>FAVORITES</span>
+          <div className="size-5 flex items-center justify-center rounded-full text-captionSm text-lime-500 bg-lime-800 pt-0.5">
+            {favorites.length}
+          </div>
+        </div>
+      ),
+      content: <Favorites favorites={favorites} />,
     },
     {
       value: "log",
-      title: "LOG",
-      content: <div>LOG CONTENT</div>,
+      title: (
+        <div className="me-6 w-full flex items-center justify-between gap-2 sm:me-0">
+          <span>LOG</span>
+          <div className="size-5 flex items-center justify-center rounded-full text-captionSm text-lime-500 bg-lime-800 pt-0.5">
+            {favorites.length}
+          </div>
+        </div>
+      ),
+      content: <Logs />,
     },
   ];
 
   return (
-    <Tabs defaultValue="history">
-      <div className="border-b border-b-neutral-600">
+    <Tabs value={selectedTab} onValueChange={setSelectedTab}>
+      <div className="hidden sm:block border-b border-b-neutral-600">
         <TabsList variant="line">
           {tabItems.map((item) => (
             <TabsTrigger key={item.value} value={item.value}>
@@ -35,6 +77,28 @@ const DetailsContainer = () => {
             </TabsTrigger>
           ))}
         </TabsList>
+      </div>
+
+      <div className="sm:hidden">
+        <Select value={selectedTab} onValueChange={setSelectedTab}>
+          <SelectTrigger className="w-full! h-10! px-3! text-bodyMd!">
+            <SelectValue />
+          </SelectTrigger>
+
+          <SelectContent position="popper">
+            <SelectGroup className="p-2!">
+              {tabItems.map((item) => (
+                <SelectItem
+                  key={item.value}
+                  value={item.value}
+                  className="text-bodyMd! py-2.5! px-2!"
+                >
+                  {item.title}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
       </div>
       {tabItems.map((item) => (
         <TabsContent key={item.value} value={item.value}>
