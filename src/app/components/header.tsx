57105ff7ee1f4ef68070230d3f86d1c0ad459e58
hale <hale.sheikhi81@gmail.com>
2026-07-11 02:40:37 +0330

    feat(ui): display dynamic number of currencies in header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
 import logo from "../../../public/images/logo.svg";
+import { getCurrencies } from "@/lib/helpers";
 
-const Header = () => {
+const Header = async () => {
+  const currencies = await getCurrencies();
+  const currenciesCount = currencies?.length || 0;
+
   return (
     <header className="flex items-center justify-between px-6 py-5">
-      <Image src={logo} alt="logo" />
+      <Image src={logo} alt="logo" className="h-5 w-auto sm:h-auto" />
 
-      <div className="text-bodySm text-neutral-200">
-        55 Currencies · EOD · ECB data
+      <div className="text-captionSm sm:text-bodySm text-neutral-200">
+        {currenciesCount} Currencies · EOD · ECB data
       </div>
     </header>
   );
