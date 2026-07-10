bb3939cfd45fa5867e74a4240dac0ac04299209b
hale <hale.sheikhi81@gmail.com>
2026-07-11 02:36:19 +0330

    refactor: update project structure and handle loading and error states

---
+++ src/app/(root)/error.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  unstable_retry,
+}: {
+  error: Error & { digest?: string };
+  unstable_retry: () => void;
+}) {
+  useEffect(() => {
+    console.log("error", error);
+  }, [error]);
+
+  return (
+    <div className="h-full flex items-center justify-center flex-col gap-2">
+      <h2>Something went wrong!</h2>
+      <Button onClick={() => unstable_retry()}>Reload</Button>
+    </div>
+  );
+}
