ca31c0eb455fe784d703c0c53699412007a0116b
hale <hale.sheikhi81@gmail.com>
2026-07-13 08:16:39 +0330

    style: handle liveMarkets responsiveness

--- src/app/components/liveMarkets.tsx
+++ src/app/components/liveMarkets.tsx
@@ -1,18 +1,19 @@
-import { getLatestPairsChanges } from "@/lib/helpers";
+import { getLatestTwoRates, getPairsChanges } from "@/lib/helpers";
 import { Ticker } from "./ticker";
 
 const LiveMarkets = async () => {
-  const rates = await getLatestPairsChanges();
+  const latestPairs = await getLatestTwoRates();
+  const rates = getPairsChanges(latestPairs);
 
   return (
     <div className="flex relative">
-      <div className="w-fit shrink-0 bg-lime-500 flex items-center gap-2 px-4 py-3">
+      <div className="w-fit shrink-0 bg-lime-500 flex items-center gap-2 px-2 sm:px-4 py-3">
         <span className="relative flex size-1.5">
           <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-neutral-300 opacity-75"></span>
           <span className="relative inline-flex size-1.5 rounded-full bg-neutral-900 pb-1.75"></span>
         </span>
 
-        <div className="text-caption font-medium text-neutral-900 uppercase">
+        <div className="text-captionSm sm:text-caption sm:font-medium text-neutral-900 uppercase">
           Live markets
         </div>
       </div>
